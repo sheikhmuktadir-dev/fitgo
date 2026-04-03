73996ba886ad386d31498c9f98d33f65e2c3e9a4
Sheikh Muktadir <your-email@example.com>
2026-04-03 12:36:21 +0530

    logo update

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -26,7 +26,7 @@ export default function Footer() {
                 <title>Barbell</title>
                 <path d="M496 176h-58v-64h-76v122H150V112H74v64H16v160h58v64h76V278h212v122h76v-64h58V176z" />
               </svg>
-              <span>Fitgo</span>
+              <span>FitGo</span>
             </Link>
             <p className={Style.footerLogoPara}>
               Etiam suscipit fringilla ullamcorper sed malesuada urna nec odio.
